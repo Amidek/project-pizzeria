9c7ef6eeb3c700df815d2cf41237d74f22c2fb98
Mariusz Jabłoński <amidek321@gmail.com>
2019-09-10 21:32:04 +0200

    stworzenie klasy booking

---
+++ src/js/components/booking.js
@@ -0,0 +1,29 @@
+import amountWidget from './amountWidget.js';
+import {templates, select} from '../settings.js';
+
+class Booking {
+  constructor(element) {
+    const thisBooking = this;
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+  render(element) {
+    const thisBooking = this;
+    const generatedHTML = templates.bookingWidget();
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+
+  }
+  initWidgets() {
+    const thisBooking = this;
+    thisBooking.peopleAmount = new amountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new amountWidget(thisBooking.dom.hoursAmount);
+  }
+
+}
+
+
+export default Booking;
